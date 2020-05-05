e882c393f902852743de0c2deff2a09007b9e5dd
keerthi133 <61080143+keerthi133@users.noreply.github.com>
2020-05-05 19:15:20 +0530

    create employee.c
    It will prints the details of employee

---
+++ employee.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+
+    char name[30];
+    char id[10];
+    int salary;
+    printf("enter the name\n");
+    scanf("%d",&name);
+    printf("enter id\n");
+    scanf("%d",&id);
+    printf("enter salary:");
+    scanf("lf",&salary);
+return 0;
+    }
+
